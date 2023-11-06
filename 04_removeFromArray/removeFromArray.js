f8699e9a767e5871a5432d0480877393666d7819
Thabo Vincent Moloi <102384087+thabomcodes@users.noreply.github.com>
2023-11-06 13:14:10 +0000

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    const argsArr = Array.from(args);
+    for (const arg of args) {
+        const i = arr.indexOf(arg);
+        if (i > -1) arr.splice(i, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
