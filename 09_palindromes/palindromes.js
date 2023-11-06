4adf0c6dd062765e9de4411ec26a1a6870918cb2
Thabo Vincent Moloi <102384087+thabomcodes@users.noreply.github.com>
2023-11-06 14:47:21 +0000

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    const s = sentence.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reverse = s.split("").reverse().join("");
+    return s === reverse;
 };
 
 // Do not edit below this line
