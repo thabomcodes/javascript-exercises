05545efbe396d520a44144ac426f6c85151aca73
Thabo Vincent Moloi <102384087+thabomcodes@users.noreply.github.com>
2023-11-06 16:02:04 +0000

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const getAge = function (person) {
+  const { yearOfBirth, yearOfDeath } = person;
+  return yearOfDeath !== undefined
+    ? yearOfDeath - yearOfBirth
+    : new Date().getFullYear() - yearOfBirth;
+};
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    if (getAge(oldestPerson) < getAge(currentPerson)) return currentPerson;
+    return oldestPerson;
+  });
 };
 
 // Do not edit below this line
