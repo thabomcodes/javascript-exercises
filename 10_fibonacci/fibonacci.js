c0c93f1425561425ef7348456ea3cf03b8db40ad
Thabo Vincent Moloi <102384087+thabomcodes@users.noreply.github.com>
2023-11-06 15:11:36 +0000

    Solve exercise 10 with iteration and recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacciRecursive = function (n) {
+    if (n <= 0) return "OOPS";
+    if (n == 1 || n == 2) return 1;
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
 
+const fibonacci = function(n) {
+    if (n <= 0) return "OOPS";
+
+    if (n == 1 || n == 2) return 1;
+
+    let prev = 1;
+    let curr = 1;
+    for (let i = 3; i <= n; i++) {
+        let newCurr = curr + prev;
+        prev = curr;
+        curr = newCurr;
+    }
+    return curr;
 };
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = {fibonacciRecursive, fibonacci};
