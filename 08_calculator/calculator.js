0b126c66e53fd4b692f674941bbad4f7efa18998
Thabo Vincent Moloi <102384087+thabomcodes@users.noreply.github.com>
2023-11-06 14:28:47 +0000

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) return 0;
+
+  let sum = 0;
+  for (const num of arr) {
+    sum += num;
+  }
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  // array.reduce can work here
+  if (arr.length === 0) return 0;
 
+  let prod = 1;
+  for (const num of arr) {
+    prod *= num;
+  }
+	return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  let fact = 1;
+  for (let i = 1; i <= a; i ++)
+    fact *= i;
+  return fact;
 };
 
 // Do not edit below this line
