b32c10edc413fbd378319c97e1bb2e6a1ea26f06
Thabo Vincent Moloi <102384087+thabomcodes@users.noreply.github.com>
2023-11-06 13:39:34 +0000

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0)
+        return "ERROR";
 
+    let i = Math.min(a, b);
+    let j = Math.max(a, b);
+
+    // using while loop
+    let sum = 0;
+    while (i <= j) {
+        sum += i;
+        i++;
+    }
+    return sum;
+    // Simple return statement 
+    // return j * (i + j) / 2; 
 };
 
 // Do not edit below this line
